bcc9fad8c7c550ce23fd7c046db4b8d151028f5b
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-23 16:10:28 +0700

    add generator

--- blocks/blocks_heartbeat.js
+++ blocks/blocks_heartbeat.js
@@ -1,15 +1,35 @@
-Blockly.Blocks['hearbeat_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("HEARBEAT");
-    this.appendValueInput("NAME")
+Blockly.Blocks["hearbeat_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("HEARBEAT");
+      this.appendValueInput("HEARBEAT_PIN")
         .setCheck("Number")
         .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+      this.appendValueInput("HEARBEAT_ALPHA")
+        .setCheck("Number")
+        .appendField("ALPHA");
+      this.appendValueInput("HEARBEAT_PERIOD")
+        .setCheck("Number")
+        .appendField("PERIOD");
+      this.appendValueInput("HEARBEAT_CHANGE")
+        .setCheck("Number")
+        .appendField("CHANGE");
+      this.appendDummyInput()
+        .appendField("RAW VALUE SET TO")
+        .appendField(
+          new Blockly.FieldVariable("HEARBEAT_RAW_VALUE"),
+          "HEARBEAT_RAW_VALUE"
+        );
+      this.appendDummyInput()
+        .appendField("VALUE SET TO")
+        .appendField(
+          new Blockly.FieldVariable("HEARBEAT_VALUE"),
+          "HEARBEAT_VALUE"
+        );
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_heartbeat.js
+++ blocks/generators_heartbeat.js
@@ -1,6 +1,49 @@
-Blockly.JavaScript['hearbeat_block'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
+module.exports = function(Blockly) {
+  Blockly.JavaScript["hearbeat_block"] = function(block) {
+    var value_hearbeat_pin = Blockly.JavaScript.valueToCode(
+      block,
+      "HEARBEAT_PIN",
+      Blockly.JavaScript.ORDER_ATOMIC
+    );
+    var value_hearbeat_alpha = Blockly.JavaScript.valueToCode(
+      block,
+      "HEARBEAT_ALPHA",
+      Blockly.JavaScript.ORDER_ATOMIC
+    );
+    var value_hearbeat_period = Blockly.JavaScript.valueToCode(
+      block,
+      "HEARBEAT_PERIOD",
+      Blockly.JavaScript.ORDER_ATOMIC
+    );
+    var value_hearbeat_change = Blockly.JavaScript.valueToCode(
+      block,
+      "HEARBEAT_CHANGE",
+      Blockly.JavaScript.ORDER_ATOMIC
+    );
+    var variable_hearbeat_raw_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("HEARBEAT_RAW_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    var variable_hearbeat_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("HEARBEAT_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#VARIABLE
+		int HEARBEAT_PIN = ${value_hearbeat_pin};
+		double HEARBEAT_ALPHA = ${value_hearbeat_alpha};
+		int HEARBEAT_PERIOD = ${value_hearbeat_period};
+		double HEARBEAT_CHANGE = ${value_hearbeat_change};
+	#END
+
+	static double HEARBEAT_OLD_VALUE = 0;
+	static double HEARBEAT_OLD_CHANGE = 0;
+	HEARBEAT_RAW_VALUE = analogRead(HEARBEAT_PIN);
+	HEARBEAT_VALUE = HEARBEAT_ALPHA * HEARBEAT_OLD_VALUE + (1 - HEARBEAT_ALPHA) * HEARBEAT_RAW_VALUE;
+	HEARBEAT_OLD_VALUE = HEARBEAT_VALUE;
+	delay(HEARBEAT_PERIOD);
+    `;
+    return code;
+  };
 };
